9f5216c1bb8df4f656ce65a81fdb87167b0f79c7
Felipe Lemos <elrond225@gmail.com>
2023-05-23 17:41:03 -0300

    Editing Colors Archive.

--- styles/index.ts
+++ styles/index.ts
@@ -1,11 +1,11 @@
 export const Colors = {
-    background: '#282729',
-    primary: '#D78F3C',
-    warning: '#FF4B4B',
-    input: '#656262',
-    inputPlaceHolder: '#DBDBDB',
-    error: '#FA2E16',
-    card: '#3C3C3E',
-    cardTitle: '#F5F5F5',
-    priceButton: '#282729',
-  }
+  background: "#282729",
+  primary: "#D78F3C",
+  warning: "#FF4B4B",
+  input: "#656262",
+  inputPlaceHolder: "#DBDBDB",
+  error: "#FA2E16",
+  card: "#3C3C3E",
+  cardTitle: "#F5F5F5",
+  priceButton: "#282729",
+};
