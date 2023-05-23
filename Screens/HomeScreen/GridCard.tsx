5950b95a0fbfaff4dbe387c9081f118ae714194a
Felipe Lemos <elrond225@gmail.com>
2023-05-23 17:39:17 -0300

    Adding The New Components to the GridCard Home Screen Component.

--- Screens/HomeScreen/GridCard.tsx
+++ Screens/HomeScreen/GridCard.tsx
@@ -1,9 +1,11 @@
-import { Text, View, FlatList, Image, Button } from "react-native";
+import { Text, View, FlatList, Image, TouchableOpacity } from "react-native";
 import styles from "./Styles";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { PriceCard } from "../../Components/PriceCard/PriceCard";
+import FavoriteButton from "../../Components/FavoriteButton/FavoriteButton";
 
-type IconData = {id: number, title: string, price: number, image: string};
+type IconData = { id: number; title: string; price: number; image: string };
 
 const GridCard = (): JSX.Element => {
   const [cardsData, setCardsData] = useState<IconData[]>([]);
@@ -21,26 +23,25 @@ const GridCard = (): JSX.Element => {
     }
   };
 
-  const renderCard = ({ item }: {item: IconData}) => (
+  const renderCard = ({ item }: { item: IconData }) => (
     <View style={styles.card}>
       <Text style={styles.productTitle}>{item.title}</Text>
-      <Image source={{uri: item.image }} style={styles.cardImage} />
-      <Text style={styles.priceButton}> R${item.price}</Text>
+      <Image source={{ uri: item.image }} style={styles.cardImage} />
+      <View style={styles.priceAndFavoriteContainer}>
+        <PriceCard priceText={"R$"} priceNumber={item.price} />
+        <FavoriteButton />
+      </View>
     </View>
   );
 
   return (
-    <View>
-      <FlatList
-        data={cardsData}
-        numColumns={2}
-        renderItem={renderCard}
-        keyExtractor={(item) => item.id.toString()}
-      />
-    </View>
+    <FlatList
+      data={cardsData}
+      numColumns={2}
+      renderItem={renderCard}
+      keyExtractor={(item) => item.id.toString()}
+    />
   );
 };
 
 export default GridCard;
-
-
