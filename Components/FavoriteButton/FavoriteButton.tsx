63965759c312a96e0d6f77ff83b2df51190d971e
Felipe Lemos <elrond225@gmail.com>
2023-05-23 17:37:20 -0300

    Adding Favorite Button Component.

---
+++ Components/FavoriteButton/FavoriteButton.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { TouchableOpacity, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Colors } from "../../styles/index";
+
+const FavoriteButton = () => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const handlePress = () => {
+    setIsFavorite(!isFavorite);
+  };
+
+  return (
+    <TouchableOpacity onPress={handlePress}>
+      <View>
+        <Ionicons
+          name={isFavorite ? "heart" : "heart-outline"}
+          size={33}
+          color={isFavorite ? Colors.cardTitle : Colors.cardTitle}
+        />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default FavoriteButton;
