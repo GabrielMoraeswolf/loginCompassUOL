1352b1ce05c28e277efb914dc1272ca933f9e10d
Felipe Lemos <elrond225@gmail.com>
2023-05-23 10:10:35 -0300

    Adding New colors

--- styles/index.ts
+++ styles/index.ts
@@ -1,9 +1,10 @@
 export const Colors = {
-    background: '#2D2D2D',
+    background: '#282729',
     primary: '#D78F3C',
     warning: '#FF4B4B',
     input: '#656262',
     inputPlaceHolder: '#DBDBDB',
     error: '#FA2E16',
-    card: '#F5F5F5'
+    card: '#3C3C3E',
+    cardTitle: '#F5F5F5'
   }
