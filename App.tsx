a0a52f19412c4742afef7ca5b861f9a4d2266c2b
Ismael Kentenich <ismaelkentenichm@gmail.com>
2023-05-23 16:50:15 -0300

    adding SafeAreView

--- App.tsx
+++ App.tsx
@@ -3,6 +3,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Navigation from './Navigation/Navigation'
+import { View } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import { Colors } from './styles';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,8 +13,10 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <>
+    <SafeAreaView style={{ flex: 1, backgroundColor: Colors.background }}>
       <StatusBar style="light" />
       <Navigation />
+    </SafeAreaView>
     </>
   );
 }
