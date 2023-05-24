3db222b01641a33722b14203262142c59d7667cb
Felipe Lemos <elrond225@gmail.com>
2023-05-24 07:18:46 -0300

    Correcting conflict with the branch.

--- Screens/HomeScreen/GridCard.tsx
+++ Screens/HomeScreen/GridCard.tsx
@@ -1,10 +1,12 @@
-import { Text, View, FlatList, Image,TouchableOpacity } from "react-native";
+import { Text, View, FlatList, Image, TouchableOpacity } from "react-native";
 import styles from "./Styles";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { PriceCard } from "../../Components/PriceCard/PriceCard";
+import FavoriteButton from "../../Components/FavoriteButton/FavoriteButton";
 import { useNavigation } from '@react-navigation/native';
 
-type IconData = {id: number, title: string, price: string, image: string};
+type IconData = { id: number, title: string, price: number, image: string };
 
 const GridCard = (): JSX.Element => {
   const [cardsData, setCardsData] = useState<IconData[]>([]);
@@ -14,7 +16,6 @@ const GridCard = (): JSX.Element => {
     fetchCardsData();
   }, []);
 
- 
   const fetchCardsData = async () => {
     try {
       const response = await axios.get("https://fakestoreapi.com/products/");
@@ -23,17 +24,21 @@ const GridCard = (): JSX.Element => {
       console.error("Error fetching data: ", error);
     }
   };
- 
-    const handleCardPress = () => {
-      navigation.navigate('ProductScreen');
-    
-  };
+
+  const handleCardPress = () => {
+    navigation.navigate('ProductScreen');
+  
+};
+
   const renderCard = ({ item }: { item: IconData }) => (
     <TouchableOpacity style={styles.card} onPress={handleCardPress}>
       <Text style={styles.productTitle}>{item.title}</Text>
       <Image source={{ uri: item.image }} style={styles.cardImage} />
-      <Text style={styles.priceButton}>R${item.price}</Text>
-    </TouchableOpacity>
+      <View style={styles.priceAndFavoriteContainer}>
+        <PriceCard priceText={"R$"} priceNumber={item.price} />
+        <FavoriteButton />
+      </View>
+      </TouchableOpacity>
   );
 
   return (
