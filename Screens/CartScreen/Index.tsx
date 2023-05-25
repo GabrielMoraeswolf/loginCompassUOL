e89480c1f2f6c20a0986c9a11a1756dc0eace129
GabrielMoraeswolf <58598993+GabrielMoraeswolf@users.noreply.github.com>
2023-05-25 09:42:03 -0300

    CartSreen test

--- Screens/CartScreen/Index.tsx
+++ Screens/CartScreen/Index.tsx
@@ -1,32 +1,25 @@
-import { ScrollView, View, Text, Image } from "react-native";
+import { ScrollView, View, Text, Image,Button } from "react-native";
 import styles from "./Styles";
+import ProductScreen from "../ProductScreen/Index"
 import { PriceCard } from "../../Components/PriceCard/PriceCard";
-import FavoriteButton from "../../Components/FavoriteButton/FavoriteButton";
 
-const ProductScreen = ({ route }: { route: any }) => {
+
+const CartScreen = ({ route }: { route: any }) => {
     const { title, price, image } = route.params;
-    return (
-      <View style={styles.container}>
-        <View style={styles.card}>
-          <Text style={styles.productTitle}>{title}
-          <FavoriteButton />
-          </Text>
-          <Image source={{ uri: image }} style={styles.cardImage} />
-          <View style={styles.priceAndFavoriteContainer}>
-            <PriceCard priceText={"R$"} priceNumber={price} />
-          </View>
+    
+    return(
+        <View style={styles.container}>
+      <View style={styles.card}>
+        <Text style={styles.productTitle}>
+          {title}
+        </Text>
+        <Image source={{ uri: image }} style={styles.cardImage} />
+        <View style={styles.priceAndFavoriteContainer}>
+          <PriceCard priceText={"R$"} priceNumber={price} />
         </View>
       </View>
-      );
-  };
-
-function CartScreen() {
-    return(
-        <ScrollView style ={styles.container}>
-            <View>
-              
-            </View>
-        </ScrollView>
+      
+    </View>
     );
 }
 
