df2beb0d40ec2416153c263c7f1cc1c03743951a
GabrielMoraeswolf <58598993+GabrielMoraeswolf@users.noreply.github.com>
2023-05-27 14:36:27 -0300

    build botton remove

--- Screens/CartScreen/Index.tsx
+++ Screens/CartScreen/Index.tsx
@@ -19,9 +19,7 @@ export const CartScreen = () => {
       );
     };
 
-    const clearCart = () => {
-      cartContext.clearCart(); 
-    };
+   
 
     const TotalPrice = () => { //sum total price function
       return cartContext.cards.reduce((total, card) => total + card.price, 0);
@@ -33,75 +31,58 @@ export const CartScreen = () => {
       cartContext.clearCart(); 
     };
 
-    const handleBuyButtonPress = () => {
-      Alert.alert(
-        "Good!",
-        "Product successfully purchased.",
-        [{ text: "OK", onPress: clearCart }],
-        { cancelable: false }
-      );
-    };
+    
     
     return(
-        <ScrollView style ={styles.container}>
-            <View style ={styles.titleTextContainer}>
-               <Text style ={styles.totalText}>TOTAL</Text>
-               <View style ={styles.priceContainer}>
-                    <Text style ={styles.priceText}>R$</Text>
-                    <Text style ={styles.priceText}>{TotalPrice().toFixed(2)}</Text>
-               </View>
-            </View>
+      <View style ={styles.container}>
+      <ScrollView >
+        <View style ={styles.textContainer}>
+          <Text style ={styles.totalText}>TOTAL</Text>
+          <View style ={styles.priceContainer}>
+            <Text style ={styles.priceText}>R$</Text>
+            <Text style ={styles.priceText}>{TotalPrice().toFixed(2)}</Text>
           </View>
-          {cartContext.cards.length === 0 ? ( 
-            <View>
-              <Text style={styles.EmptyCartTextUp}>Ops, Empty Cart :{"("}</Text> 
-              <Text style={styles.EmptyCartTextDown}>Add a product</Text>
-            </View> ) : (
-            <View>
-              {/* Render cards */}
-              {cartContext.cards.map((card, index) => (  
-                <View key={index} style={styles.card}>
-                  <View style={styles.buttonRemove}>
+        </View>
+        {cartContext.cards.length === 0 ? ( 
+          <View>
+            <Text style={styles.EmptyCartTextUp}>Ops, Empty Cart :{"("}</Text> 
+            <Text style={styles.EmptyCartTextDown}>Add a product</Text>
+          </View> ) : (
+          <View>
+            {/* Render cards */}
+            {cartContext.cards.map((card, index) => (  
+              <View key={index} style={styles.card}>
+       
+       <View style={styles.buttonRemove}>
                     <ButtonRemove onPress={() => removeCard(card.id)}/>
                   </View>
-                  <View /* style={styles.cardRender} */>
-
-                    <View style={styles.imageAndTextContainer}> 
-                      <View style={styles.imageContainer}>
-                        <Image
-                          source={{ uri: card.image }}
-                          style={styles.cardImage}
-                          resizeMode="contain"
-                        /> 
-                      </View>
-                      <View style={styles.textAndPriceContainer}>
-                        <View style={styles.textContainer}>
-                          <Text style={styles.productTitle} numberOfLines={2}>
-                            {card.title}
-                          </Text>
-                        </View> 
-
-                        <View style={styles.priceCardContainer}>
-                          <PriceCardHome /* style={styles.priceCard} */ priceText={"R$"} priceNumber={card.price} />
-                        </View>
-
-                      </View> 
-
-                    </View>
-
-                  </View> 
-
-                  <View style={styles.line} />
-                </View>                   
-              ))} 
-              
-              
-            </View>
-            
-          )}
-      
+                
+               
+              <Text style={styles.productTitle} numberOfLines={2}>
+                {card.title}
+              </Text>
+              <View style={styles.imageContainer}>
+                <Image
+                  source={{ uri: card.image }}
+                  style={styles.cardImage}
+                  resizeMode="contain"
+                /> 
+              </View>
+              <View style={styles.priceCardContainer}>
+                <PriceCardHome style={styles.priceCard} priceText={"R$"} priceNumber={card.price} />
+              </View>
+                <View style={styles.line} />
+            </View>        
+          ))}            
+        </View>
+      )}
+      </ScrollView>
       <View style={styles.buttonContainer}>
         <PrimaryButton onPress={handleBuyButtonPress}>BUY</PrimaryButton>
-      </View>
-    );
+      </View>  
+    </View>
+  );
 }
+
+
+
