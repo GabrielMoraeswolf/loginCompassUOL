d750fb9d7e1aae5152bfe64f8d2200ca99c14e93
Felipe Lemos <elrond225@gmail.com>
2023-05-27 20:05:44 -0300

    Last update on CartButton.

--- Components/CartButton/CartButton.tsx
+++ Components/CartButton/CartButton.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { TouchableOpacity, View, Image, Text, StyleSheet } from 'react-native';
 import styles from './Styles';
+import { CartContext } from '../Context/CartContext';
 
-function CartButton({ onPress }: any) {
+function CartButton({ onPress, cartCount }: any) {
+  const cartCounter = useContext(CartContext);
   return (
     <TouchableOpacity onPress={onPress}>
       <Image source={require('../../assets/cart-icon.png')} style={styles.cartButton} />
       <View style={styles.badge}>
-      {/* DEIXAR DINAMICO APÓS TER O CARTSCREEN */}
-        <Text style={styles.badgeText}>2</Text> 
+        <Text style={styles.badgeText}>{cartCounter.cards.length}</Text> 
       </View>
     </TouchableOpacity>
   );
