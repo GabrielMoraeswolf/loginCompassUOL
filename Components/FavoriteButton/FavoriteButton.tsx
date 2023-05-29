0fe4697d66ba79dea9833ccad769a384daa43f2f
Ismael Kentenich <ismaelkentenichm@gmail.com>
2023-05-29 15:49:44 -0300

    favoritebutton function

--- Components/FavoriteButton/FavoriteButton.tsx
+++ Components/FavoriteButton/FavoriteButton.tsx
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import { TouchableOpacity, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Colors } from "../../styles/index";
+import { FavoriteContext } from "../Context/FavoriteContext";
 
-type Props ={
-  size: number,
-}
-const FavoriteButton = ({size}:Props) => {
-  const [isFavorite, setIsFavorite] = useState(false);
+type Props = {
+  size: number;
+  productId: number;
+};
+
+const FavoriteButton = ({ size, productId }: Props) => {
+  const { favorites, toggleFavorite } = useContext(FavoriteContext);
+
+  const isFavorite = favorites.includes(productId); // <-- Adicione o ID do produto aqui
 
   const handlePress = () => {
-    setIsFavorite(!isFavorite);
+    toggleFavorite(productId); // <-- Adicione o ID do produto aqui
   };
 
   return (
